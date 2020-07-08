79e94665fdb311187fb527f8656f2c795fd2d123
viocuong <viocuong@users.noreply.github.com>
2020-07-09 02:56:25 +0700

    up

--- public/js/main.js
+++ public/js/main.js
@@ -1,12 +1,4 @@
 $(document).ready(function() {
-    $("#userdk").keyup(function() {
-        var user = $(this).val();
-        $.post("./ajax/checkuser", {
-            ur: user
-        }, function(data) {
-            $("#messuser").html(data);
-        });
-    });
     $("#viewproduct").click(function() {
         $.post("./ajax/viewAllProduct", function(data) {
             $("#listproduct").html(data);
